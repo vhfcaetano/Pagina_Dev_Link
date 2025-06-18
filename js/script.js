6a76e517e2749fad86e60fd554b317d3c7339140
Victor Hugo Fonseca Caetano <victorhugofcaetano99@gmail.com>
2025-06-18 12:07:16 -0300

    update nuncajoguei

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ function toggleMode() {
   const image = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    image.setAttribute("src", "./assets/avatar-light.png");
+    image.setAttribute("src", "./assets/CORRENDO.png");
     image.setAttribute(
       "alt",
       "Foto de perfil de Victor, blusa Azul e com um sorriso simpático"
     );
   } else {
-    image.setAttribute("src", "./assets/avatar.png");
+    image.setAttribute("src", "./assets/COZY.png");
   }
 }
